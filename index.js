d8553cae0b25452d0b212f507ad0d3a697cc26af
Vladimir Duck <vanducpham2k3nh@gmail.com>
2025-03-24 19:13:41 +0700

    connect to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
+const database = require("./config/database");
 const route = require("./routes/client/index.route");
 const app = express();
 const port = process.env.PORT;
 
+database.connect();
 
 app.set("view engine", "pug");
 app.set("views", "./views");
