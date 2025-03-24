64a7ff87a6c3d1d2dbf724ae39b832b1498d2412
Vladimir Duck <vanducpham2k3nh@gmail.com>
2025-03-24 17:20:31 +0700

    Configure PUG  directory

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,5 @@
 module.exports.index = (req, res) => {
-    res.render("client/pages/products/index");
+    res.render("client/pages/products/index",{
+        pageTitle: "Trang danh sách sản phẩm"
+    });
 }
