a8d6773405ec7f2a4d8d1634a6938e24f6bccfae
Vladimir Duck <vanducpham2k3nh@gmail.com>
2025-03-27 20:30:37 +0700

    change multi

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 require("dotenv").config();
 const mongoose = require("mongoose");
@@ -7,7 +8,9 @@ const route = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
 const app = express();
 const port = process.env.PORT;
+
 app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const systemConfig = require("./config/system");
 
--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -19,3 +19,62 @@ if (buttonChangeStatus.length > 0) {
     })
 }
 // End change status of product
+
+
+// Checkbox Multi
+const checkBoxMulti = document.querySelector("[checkbox-multi]");
+if (checkBoxMulti) {
+    const inputCheckAll = checkBoxMulti.querySelector("input[name='check-all']");
+    const inputsId = checkBoxMulti.querySelectorAll("input[name='id']");
+
+    inputCheckAll.addEventListener("click", () => {
+        if (inputCheckAll.checked) {
+            inputsId.forEach(input => {
+                input.checked = true;
+            })
+        } else {
+            inputsId.forEach(input => {
+                input.checked = false;
+            })
+        }
+    })
+
+    inputsId.forEach(input => {
+        input.addEventListener("click", () => {
+            const countChecked = checkBoxMulti.querySelectorAll("input[name='id']:checked").length;
+            if (countChecked == inputsId.length) {
+                inputCheckAll.checked = true;
+            } else {
+                inputCheckAll.checked = false;
+            }
+        })
+    })
+}
+// End Checkbox Multi
+
+// Form change multi
+const formChangeMulti = document.querySelector("[form-change-multi]");
+if (formChangeMulti) {
+    formChangeMulti.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        const checkBoxMulti = document.querySelector("[checkbox-multi]");
+        const inputsChecked = checkBoxMulti.querySelectorAll("input[name='id']:checked");
+
+        if (inputsChecked.length > 0) {
+            let ids = [];
+            const inputIds = document.querySelector("input[name='ids']");
+
+            inputsChecked.forEach(input => {
+                const id = input.value;
+                ids.push(id);
+            })
+            inputIds.value = ids.join(",");
+
+            formChangeMulti.submit();
+        } else {
+            alert("Vui lòng chọn ít nhất 1 sản phẩm");
+        }
+    });
+}
+// End form change multi
