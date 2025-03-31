64ebf253849702aa150a926a63015d8bdf7859c8
Vladimir Duck <vanducpham2k3nh@gmail.com>
2025-03-31 14:51:24 +0700

    update preview image

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -51,3 +51,35 @@ if (buttonPagination) {
     }) 
 }
 // End Button Pagination
+
+// Show alert
+const showAlert = document.querySelector("[show-alert]");
+console.log(showAlert);
+if (showAlert) {
+    const time = parseInt(showAlert.getAttribute("data-time"));
+    const closeAlert = showAlert.querySelector("[close-alert]");
+
+    setTimeout(() => {
+        showAlert.classList.add("alert-hidden");
+    }, time);
+
+    closeAlert.addEventListener("click", () => {
+        showAlert.classList.add("alert-hidden");
+    })
+}
+// End Show alert
+
+// Upload image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+    const uploadImageInput = document.querySelector("[upload-image-input]");
+    const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+    uploadImageInput.addEventListener("change", (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            uploadImagePreview.src = URL.createObjectURL(file);
+        }
+    });
+}
+// End Upload image
