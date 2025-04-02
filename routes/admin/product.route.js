1a04d7662792774c9aeaf225f8d65974700b8fe6
Vladimir Duck <vanducpham2k3nh@gmail.com>
2025-04-02 17:54:26 +0700

    upload image to cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const multer = require("multer");
 const router = express.Router();
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
@@ -12,7 +12,7 @@ router.patch("/change-status/:status/:id", controller.changeStatus);
 router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
-router.post("/create", upload.single("thumbnail"), validate.createPost, controller.createPost);
+router.post("/create", upload.single("thumbnail"), uploadCloud.uploadCloud, validate.createPost, controller.createPost);
 
 router.get("/edit/:id", controller.edit);
 router.patch("/edit/:id", upload.single("thumbnail"), validate.createPost, controller.editPatch);
