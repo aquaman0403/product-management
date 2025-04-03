c8ffc3f818fbf7d3d737d13d394105b51f0a6489
Vladimir Duck <vanducpham2k3nh@gmail.com>
2025-04-03 23:05:16 +0700

    tiny mce

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const cookieParser = require('cookie-parser')
 const session = require("express-session");
 require("dotenv").config();
-const mongoose = require("mongoose");
-const multer = require("multer");
 const database = require("./config/database");
 const route = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
@@ -30,6 +29,8 @@ app.use(cookieParser("Dupham2003bgHehehehehe"));
 app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
 
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+
 // public
 app.use(express.static(`${__dirname}/public`));
 
@@ -39,4 +40,11 @@ routeAdmin(app);
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
-})
+}).on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+        console.error(`Port ${port} is already in use. Please use a different port.`);
+        process.exit(1);
+    } else {
+        throw err;
+    }
+});
