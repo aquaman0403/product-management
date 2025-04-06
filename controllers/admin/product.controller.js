38ccd1634350839188a60a3df1ec7de84975d298
Vladimir Duck <vanducpham2k3nh@gmail.com>
2025-04-06 23:15:33 +0700

    update product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,8 +1,9 @@
 const Product = require("../../models/product.model");
-
+const ProductCategory = require("../../models/product-category.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
+const { tree } = require("../../helpers/createTree");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -128,8 +129,17 @@ module.exports.deleteItem = async (req, res) => {
 
 // [GET] /admin/products/create
 module.exports.create = async (req, res) => {
+    let find = {
+        deleted: false
+    }
+
+    const categories = await ProductCategory.find(find)
+
+    const newCategories = tree(categories)
+
     res.render("admin/pages/products/create", {
         pageTitle: "Thêm sản phẩm mới",
+        categories: newCategories
     })
 }
 
