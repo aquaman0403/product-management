027cc9f9dad2d772702d2a0a3c3c38b134417872
Vladimir Duck <vanducpham2k3nh@gmail.com>
2025-04-08 01:17:12 +0700

    update

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -58,3 +58,28 @@ module.exports.editPatch = async (req, res) => {
 
     res.redirect(`/admin/roles`);
 }
+
+module.exports.permissions = async (req, res) => {
+    let find = {
+        deleted: false
+    }
+
+    const records = await Role.find(find)
+
+    console.log(records)
+
+    res.render("admin/pages/roles/permissions", {
+        pageTitle: "Phân quyền",
+        records: records
+    })
+}
+
+module.exports.permissionsPatch = async (req, res) => {
+    const permissions = JSON.parse(req.body.permissions)
+    for (const item of permissions) {
+
+        await Role.updateOne({_id: item.id}, {permissions: item.permissions})
+    }
+
+    res.redirect("back")
+}
